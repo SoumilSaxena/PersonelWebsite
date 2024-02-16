c7042b170a83a82c08d6d2af1ef8c756350dc7be
SoumilSaxena <soumil@usf.edu>
2024-02-17 01:33:31 +0400

    Introduction, Section Divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Introduction from "@/components/introduction";
+import Sectiondivider from "@/components/sectiondivider";
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Introduction />
+      <Sectiondivider />
+    </main>
+  );
 }
---
+++ components/introduction.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import React from "react";
+import profile_pic from "@/public/profilepic.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+export default function Introduction() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={profile_pic}
+              alt="Soumil portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-30 w-30 rounded-full object-cover border-[0.35rem] border-white shadow-x1"
+            />
+          </motion.div>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hi, I'm Soumil Saxena.</span> I'm passionate
+        about <span className="italic">technology</span> and{" "}
+        <span className="font-bold underline">Full-Stack Development</span>.
+        Eager to apply my knowledge and creativity, I aim to craft seamless
+        digital solutions. Let's{" "}
+        <span className="font-bold">innovate and build</span> together.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-100 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV.pdf"
+          download
+        >
+          Download Resume{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/soumil-saxena"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700  hover:text-gray-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/SoumilSaxena"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
