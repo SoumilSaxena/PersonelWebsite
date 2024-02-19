111830dca09efd00192b185086af267bb74d340b
SoumilSaxena <soumil@usf.edu>
2024-02-20 00:21:31 +0400

    experience finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import About from "@/components/about";
+import Experience from "@/components/experience";
 import Introduction from "@/components/introduction";
 import Projects from "@/components/projects";
 import Sectiondivider from "@/components/sectiondivider";
 import Sectiondivider2 from "@/components/sectiondivider2";
+import Skills from "@/components/skills";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
@@ -10,8 +12,10 @@ export default function Home() {
       <Sectiondivider />
       <About />
       <Sectiondivider2 />
-
       <Projects />
+      <Sectiondivider />
+      <Skills />
+      <Experience />
     </main>
   );
 }
