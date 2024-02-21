fd943aad930ff6105f4d5b356cebb145c315f434
SoumilSaxena <soumil@usf.edu>
2024-02-21 14:40:52 +0400

    update about

--- components/about.tsx
+++ components/about.tsx
@@ -19,7 +19,7 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
 
-      <p className="mb-3">
+      {/* <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Computer Science</span>, I decided to
         pursue my passion for programming. I learned{" "}
@@ -44,6 +44,60 @@ export default function About() {
         am currently learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
         learning how to play the guitar.
+      </p> */}
+      <p className="mb-3">
+        As a recent graduate from the{" "}
+        <span className="font-medium">University of South Florida</span>,
+        majoring in <span className="font-medium">Computer Science</span> with a
+        minor in <span className="font-medium">Mathematics</span>, I'm on the
+        lookout for a <span className="font-medium">full-time position</span>{" "}
+        where I can put my skills to work. I'm fully open to relocating anywhere
+        within the United States, ready to embark on new challenges and
+        opportunities.
+      </p>
+      <p className="mb-3">
+        I am your one-stop expert for{" "}
+        <span className="font-medium">full-stack development</span> needs,
+        equipped to transform your visionary tech concepts into reality. Whether
+        it's designing polished, user-centric websites for businesses or
+        developing cutting-edge software solutions, my skill set covers the
+        gamut from{" "}
+        <span className="font-medium">
+          JavaScript, React.js, Angular, and Node.js
+        </span>{" "}
+        to <span className="font-medium">Express.js, CSS, Next.js,</span> and{" "}
+        <span className="font-medium">LangChain</span>. My expertise lies in
+        crafting intuitive interfaces, architecting robust RESTful APIs, and
+        integrating diverse external services with ease.
+      </p>
+      <p className="mb-3">
+        As a technically proficient and innovative{" "}
+        <span className="font-medium">Software Engineer</span>, I have garnered
+        experience in software development, coding, quality assurance, and
+        operational enhancement. My proactive approach to troubleshooting,
+        debugging, and system upgrades has consistently driven project success
+        and technological advancement.
+      </p>
+      <p>
+        Beyond the world of code, I'm an avid{" "}
+        <span className="italic">
+          tennis player, chess enthusiast, and guitar player
+        </span>
+        , finding joy in the strings and strategies of each. My passion for{" "}
+        <span className="font-medium">
+          20th-century cinema and documentaries
+        </span>{" "}
+        fuels my cinephile tendencies, while my interests in{" "}
+        <span className="font-medium">
+          evolution, philosophy, and historical figures and events
+        </span>{" "}
+        keep me constantly exploring and learning.
+      </p>
+      <p>
+        In essence, I am a dedicated professional eager to leverage my technical
+        expertise and creative problem-solving skills in a dynamic work
+        environment. Let's connect and explore how I can contribute to your team
+        and bring our shared goals to fruition.
       </p>
     </motion.section>
   );
